41ec67f1e3c39a1042902cb1558c96819fb92bfa
Eraster <jonas.odermatt@gmx.ch>
2013-07-10 14:25:28 +0200

    Unfinished 3
    Ive added some details.
    For Unfinished 4 I will make the ptrogram faster and a bit less
    complicated.

--- Soap_Bubble.py
+++ Soap_Bubble.py
@@ -3,10 +3,10 @@ from random import*
 
 #-------------------------------------------------------------
 
-i = 3 # Werte in x Richtung
-j = 3 # Werte in y Richtung
+i = 20 # Werte in x Richtung
+j = 20 # Werte in y Richtung
 
-p = 100 # randint maximum
+p = 10000 # randint maximum
 q = 0 # randint minimum
 
 z = 0.1 # Abweichung der Annäherung average(n)-4*n
@@ -28,20 +28,22 @@ for a in range(0, i):
     v.append(u)
 k.append(v)
 
-print(k)
+#print(k)
         
-print("Input")
+print("Input (Berechnungstiefe: 0 )")
 for a in range(0, i):
     for b in range(0, j):
         print("(", a + 1, ", ", b + 1, ") =", k[h][a][b])
 
 #-------------------------------------------------------------
 
-while r > z :
+while z*((i - 2)*(j - 2)) < r :
 
+    #print(k[h])
+    
     h += 1
 
-    #print("k = ", h)
+    #print("Zwischenschritt (Berechnungstiefe: ", h, " )")
 
     for a in range(0, i):
         for b in range(0, j):
@@ -61,17 +63,25 @@ while r > z :
     for a in range(1, i - 1):
         for b in range(1, j - 1):
             e = (k[h][a + 1][b] + k[h][a - 1][b] + k[h][a][b + 1] + k[h][a][b - 1]) / 4
-            f = sqrt(e*e)- sqrt(k[h][a][b]*k[h][a][b])
-            r = r + f
-    r = r/((i-2)*(j-2))
+            f = sqrt(e*e) - sqrt((k[h][a][b])*(k[h][a][b]))
+            if f <= z:
+                r += f
+
+            else:
+                r = p*i*j
+    
+            
             
             
 
 else:
-    print("Berechnungstiefe: ", h)
+    #print(k)
+
+    print("Output (Berechnungstiefe: ", h," )")
     for a in range(0, i):
         for b in range(0, j):
             print("(", a + 1, ", ", b + 1, ") =", k[h][a][b])
+    print("Output (Berechnungstiefe: ", h," )")
     
             
 
